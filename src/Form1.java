62225349fb0375d721ef5360c1971c78aed87f82
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2021-10-17 17:57:34 +0400

    1

---
+++ src/Form1.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Form1 extends JFrame{
+    private JLabel HelloLabel;
+    private JPanel Panel;
+    private JButton CreateButton;
+
+    public Form1() {
+        setContentPane(Panel);
+        setVisible(true);
+        setSize(900, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CreateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HelloLabel.setText("Пока");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Form1();
+    }
+}
